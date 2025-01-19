6b199d1b75fe3993bfc3d81a316a35f02f29e73f
Elmer Marquez <leomarqz2020@gmail.com>
2025-01-19 00:13:00 -0600

    version 2 del chat

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use chrono::prelude::*;
+use tokio::sync::broadcast;
 
 const HOST: &str = "127.0.0.1:8080"; // The server address
 
@@ -11,51 +12,74 @@ async fn main() {
     // Print the server address
     println!("Server listening on: {}", HOST);
 
+    // Create a message counter
     let mut message_counter: i32 = 0;
     
     // Create a TCP listener bound to the server address
     let server_listener = TcpListener::bind(HOST).await.unwrap();
 
-    // Accept incoming connections
-    let (mut socket, mut _address ) = server_listener.accept().await.unwrap();
+    // Create a broadcast channel: 10 is the buffer size
+    let (tx, _rx) = broadcast::channel::<String>(10);
 
-    // Split the socket into reader and writer
-    let (reader, mut writer) = socket.split();
-
-    println!("Accepted connection from: {}", _address);
-
-    // Create a buffer reader
-    let mut buffer_reader = BufReader::new(reader);
-
-    // Create a buffer to store the incoming data
-    let mut line = String::new();
-
+    // Spawn a new task to handle the incoming connections
     loop {
-        // Read the incoming data
-        let _bytes = buffer_reader.read_line(&mut line).await.unwrap();
-
-        // If the incoming data is empty, break the loop
-        if _bytes == 0 {
-            println!("Connection closed...............................");
-            break;
-        }
         
-        // Response message
-        let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
-        line = format!("\n|{} | Message [{}]: {}", datetime, message_counter, line);
+        // Accept incoming connections
+        let (mut socket, mut _address ) = server_listener.accept().await.unwrap();
+    
+        // Clone the broadcast channel
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        println!("{}", line);
+        // Spawn a new task to handle the incoming connection
+        tokio::spawn(async move {
+            
+            // Split the socket into reader and writer
+            let (reader, mut writer) = socket.split();
         
-        // Print the incoming data
-        writer.write_all(line.as_bytes() ).await.unwrap();
+            println!("Accepted connection from: {}", _address);
+        
+            // Create a buffer reader
+            let mut buffer_reader = BufReader::new(reader);
+        
+            // Create a buffer to store the incoming data
+            let mut line = String::new();
 
+            
+            loop {
+                // Read the incoming data
+                let _bytes = buffer_reader.read_line(&mut line).await.unwrap();
+                
+                // If the incoming data is empty, break the loop
+                if _bytes == 0 {
+                    println!("Connection closed...............................");
+                    break;
+                }
 
-        // Clear the buffer
-        line.clear();
+                // Send the broadcast message
+                tx.send( line.clone() ).unwrap();
 
-        // Increment the message counter
-        message_counter += 1;
-
+                let mut message = rx.recv().await.unwrap();
+                
+                // Response message
+                let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
+                message = format!("\n|{} | Message [{}]: {}", datetime, message_counter, line); 
+        
+                println!("{}", message);
+                
+                // Print the incoming data
+                writer.write_all(message.as_bytes() ).await.unwrap();
+        
+                // Clear the buffer
+                line.clear();
+        
+                // Increment the message counter
+                message_counter += 1;
+        
+            }
+        });
     }
+
+    
 
 }
